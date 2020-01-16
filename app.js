4f609c2f8c71c866d9bd340dfa68c9849c3fcfd3
Busch <josefine.busch@posteo.de>
2020-01-16 16:23:07 +0100

    sw troubleshoot

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 //see if service worker is available at navigator
 if ('serviceWorker' in navigator) {
     // register service worker after load, because otherwise the sw registration would delay the initial page load
-    window.addEventListener('load', function() {
+    // window.addEventListener('load', function() {
         // the location of the service worker is important for the scope. For example, which files can be cached
       navigator.serviceWorker.register('/sw.js');
-    });
+    // });
   }
 //   end of service worker registration
 
