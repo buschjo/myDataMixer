18696bf6728aa8d3a458259ad07551d596e1b3d1
Busch <josefine.busch@posteo.de>
2020-01-28 17:37:50 +0100

    links updated

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 //adapted from https://codelabs.developers.google.com/codelabs/your-first-pwapp/ by Google Developers accessed 13.11.2019
 //Service Worker is in the root directory of the project so everything is in scope
 
-const version = 31;
+const version = 32;
 
-const CACHE_NAME = 'static-cache-v28';
+const CACHE_NAME = 'static-cache-v29';
 const FILES_TO_CACHE = [
 	'index.html',
 	'images/favicon.png',
