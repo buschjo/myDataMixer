9a769589ba4a2964018cc9b90ae41fa93d23fdc7
Busch <josefine.busch@posteo.de>
2020-01-29 18:46:43 +0100

    reorganize javascript files

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 //adapted from https://codelabs.developers.google.com/codelabs/your-first-pwapp/ by Google Developers accessed 13.11.2019
 //Service Worker is in the root directory of the project so everything is in scope
 
-const version = 34;
+const version = 35;
 
-const CACHE_NAME = 'static-cache-v31';
+const CACHE_NAME = 'static-cache-v32';
 const FILES_TO_CACHE = [
 	'index.html',
 	'images/favicon.png',
